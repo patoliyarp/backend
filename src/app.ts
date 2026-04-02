692feeff900f6f23aaeb4ee08e29a682f3b3f080
patoliyarp <patoliya.rp@gmail.com>
2026-04-02 18:02:24 +0530

    feat : add rate limit and security headers

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { errorMiddleware } from "./middleware/error.middleware";
 import { invalidRouteMiddleware } from "./middleware/invalidRoute.middleware";
 import { requestLoggerMiddleware } from "./middleware/requestLogger.middleware";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(helmet());
 
 //Request Logger middleware
 app.use(requestLoggerMiddleware);
@@ -18,9 +20,15 @@ import userRoute from "./routes/user/user.route";
 import postRoute from "./routes/posts/port.route";
 import postsRouter from "./routes/posts/posts.route";
 import streamRouter from "./routes/stream/stream.route";
+import {
+  authLimiter,
+  publicLimiter,
+  rateLimitMiddleware,
+} from "./middleware/rateLimit.middleware";
 
 //Declare routes
-app.use("/api/user", userRoute);
+app.use("/api/user", authLimiter, userRoute);
+app.use(rateLimitMiddleware);
 app.use("/api/post", postRoute);
 app.use("/api/v2/post", postsRouter);
 app.use("/api/stream", streamRouter);
--- src/routes/posts/port.route.ts
+++ src/routes/posts/port.route.ts
@@ -9,11 +9,14 @@ import {
 } from "../../controller/posts/post.controller";
 import { validateData } from "../../middleware/validation.middleware";
 import { postSchema } from "../../schema/postSchema";
+import { postValidationRules } from "../../schema/expressValidate";
+import { expressValidation } from "../../middleware/expressValidater.middleware";
 const router = express.Router();
 
 router.get("/", getPost);
 router.get("/getall", getAllPost);
-router.post("/addpost", validateData(postSchema), addPost);
+// router.post("/addpost", validateData(postSchema), addPost);
+router.post("/addpost", postValidationRules, expressValidation, addPost);
 router.patch("/updatepost/:id", updatePost);
 router.delete("/deletepost/:id", deletePost);
 router.get("/getheavypost", getHeavyPost);
