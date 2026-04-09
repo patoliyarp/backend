aa7c87afd73ed284d535fb77faaaa452942efc3b
patoliyarp <patoliya.rp@gmail.com>
2026-04-09 12:50:17 +0530

    Fix: change build script for deploy and add online redis

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,5 +1,17 @@
-export const connection = {
-  host: "127.0.0.1",
-  post: 6379,
-};
+import { configDotenv } from "dotenv";
+import Redis from "ioredis";
 
+configDotenv();
+function createConnection(): Redis {
+  return new Redis({
+    host: process.env.REDIS_HOST || "localhost",
+    port: parseInt(process.env.REDIS_PORT || "6379"),
+    password: process.env.REDIS_PASS,
+    // Critical settings for BullMQ
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
+  });
+}
+
+// Shared connection for all components
+export const connection = createConnection();
